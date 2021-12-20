87dec540a373b30b415039d49e51dba019c62fd0
Rafidah Binti Muhamad <rafidah.muhamad@paynet.my>
2021-12-20 11:48:19 +0800

    Format Issue is Fixed

--- calc.js
+++ calc.js
@@ -14,7 +14,7 @@ let division = function (x,y){
     return x / y;
 }
 
-console.log(addition(20,10))
-console.log(substraction(20,10))
-console.log(multiplication(20,10))
-console.log(division(20,10))
+console.log("Addition:", addition(20,10))
+console.log("Substraction:", substraction(20,10))
+console.log("Multiplication:", multiplication(20,10))
+console.log("Division:", division(20,10))
