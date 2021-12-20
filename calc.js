1d97d158ed9af946f1f7596f9d0ea2d528ddd35a
Rafidah Binti Muhamad <rafidah.muhamad@paynet.my>
2021-12-20 11:16:44 +0800

    First Commit for NodeJS Application

---
+++ calc.js
@@ -0,0 +1,20 @@
+let addition = function (x,y){
+    return x + y;
+}
+
+let substraction = function (x,y){
+    return x - y;
+}
+
+let multiplication = function (x,y){
+    return x * y;
+}
+
+let division = function (x,y){
+    return x / y;
+}
+
+console.log(addition(20,10))
+console.log(substraction(20,10))
+console.log(multiplication(20,10))
+console.log(division(20,10))
